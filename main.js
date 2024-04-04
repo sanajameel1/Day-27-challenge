81ea7969e0deaea4cb862dc0bfdaa5c2513370f1
sana <161457792+sanajameel1@users.noreply.github.com>
2024-04-04 15:51:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// QUESTiON 79
+// This sets up an object for a car with specific details
+// let car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+// };
+// // Accessing and showing the car's model
+// console.log(car.model); // Outputs: Corolla
+// // We use dot notation (car.model) to get the model of the car from our object.
+// QUESTION 80
+// interface car {
+//     make: string;
+//     model: string;
+//     year: number;
+//     color?: string;
+// }
+// let car: car = {
+//     make: 'ford',
+//     model: 'mustang',
+//     year: 2022
+// };
+// car.color = "white";
+// car.year = 2023;
+// console.log(car);
+// QUESTION 81
+// This function shows every detail about an object
+function logObjectProperties(obj) {
+    for (var property in obj) {
+        // Loops through each property in the object
+        console.log('${property}: ${obj[property]}');
+        // Shows the property name and its value
+    }
+}
+// Using the function with a car object
+logObjectProperties({ make: "ford", model: "mustage", year: 2021, color: "white" });
+// It tells us each piece of information stored about the car.
